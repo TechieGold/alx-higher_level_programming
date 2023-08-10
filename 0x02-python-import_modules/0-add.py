94c31c7ded48b5cb8c20f81594a3b2462f0ba176
Gold Israel <techiegold101@gmail.com>
2023-08-10 14:14:28 -0700

    Restricts execution when imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
-sum = add(a, b)
+    from add_0 import add
 
-print(f"{a} + {b} = {sum}")
+    a = 1
+    b = 2
+    sum = add(a, b)
+
+    print(f"{a} + {b} = {sum}")
