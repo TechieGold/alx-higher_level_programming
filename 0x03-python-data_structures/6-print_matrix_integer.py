26ef3b6a0a25ebf3d0096420536557e4ee7e9089
Gold Israel <techiegold101@gmail.com>
2023-08-15 04:11:37 -0700

    Add $ to be printed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(num), end=" ")
             if idx < len(row) - 1:
                 print(" ", end="")
-        print()
+        print("$")
