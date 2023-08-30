820cd491f413efa782e1123fb8e6ecd20f2d24ee
Gold Israel <techiegold101@gmail.com>
2023-08-30 08:38:01 -0700

    Add a class Square that defines a square with a size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class square
+
+   with an instance attribute:size
+"""
+
+
+class Square:
+    """Square class with size attribute"""
+    def __init__(self, size):
+        self.__size = size
