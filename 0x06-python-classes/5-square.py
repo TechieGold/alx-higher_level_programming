7969c365409d5bc14fe77e860bbe8dbe8750efb1
Gold Israel <techiegold101@gmail.com>
2023-08-30 09:48:29 -0700

    Implemented Square class with size property, area method, and print functionality

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Get or set the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The new size value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Print the square using '#'.
+
+        If size is 0, prints an empty line.
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
