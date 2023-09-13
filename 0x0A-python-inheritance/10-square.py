8b6a1ecef2767402c6fb60dd94f1e195c12d5de3
TechieGold <techiegold101@gmail.com>
2023-09-13 14:25:09 +0100

    format doctsring to fit project requirement

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
-
-
-class Square(Rectangle):
-    """
-    A square class that inherits from Rectangle class.
+"""
+    This module contain a square class that inherits from Rectangle class.
 
     methods:
         __init__(self, size): Initializes a size attribute.
     Attributes:
         __size(private): size of the square. must be an integer.
-        area(self): calculates and returns the area of the rectangle.
     """
+
+
+class Square(Rectangle):
+    """square class that inherits from Rectangle class."""
     def __init__(self, size):
+        """Initialises width and height attributes."""
         self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
