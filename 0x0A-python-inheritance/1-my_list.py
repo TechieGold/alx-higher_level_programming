61aa4b95c8e94a8d4665168bae376b6130a70747
TechieGold <techiegold101@gmail.com>
2023-09-13 13:51:50 +0100

    format doctsring to fit requirement

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-class MyList(list):
-    """
-    A custom list class that inherits from the built-in list class.
+"""
+    This module create custom list class
+    that inherits from the built-in list class.
+
+    class: MyList(list)
 
     Public Methods:
         printed_sorted(self): prints the list in ascendding sorted order.
@@ -9,6 +11,11 @@ class MyList(list):
     Attributes:
         Inherits all methods and attributes from the built-in list class.
     """
+
+
+class MyList(list):
+    """custom list class that inherits from the built-in list class"""
+
     def print_sorted(self):
         """ Prints element of the list in ascending order"""
         sorted_list = sorted(self)
