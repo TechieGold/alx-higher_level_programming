b77fac8e75a0d7d3e2e858219b4ed97a57189318
TechieGold <techiegold101@gmail.com>
2023-09-13 14:02:17 +0100

    format docstring to fit requirement

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-A BaseGeometry class with public instance method.
+This module contains a BaseGeometry class with public instance method.
 
-    methods:
-        area(self): raises an Exception with a message.
-        integer_validator(self, name, value): Validates a value.
-            if value is not an integer, a TypeError is raised.
-            if value is less than or equal to zero, a ValueError is raise.
-            Args:
-                name: name of integer(string)
-                value: of the integer (must be an integer)
+methods:
+    area(self): raises an Exception with a message.
+    integer_validator(self, name, value): Validates a value.
+        if value is not an integer, a TypeError is raised.
+        if value is less than or equal to zero, a ValueError is raise.
+Args:
+    name: name of integer(string)
+    value: of the integer (must be an integer)
 """
 
 
 class BaseGeometry:
+    """A BaseGeometry class with public instance methods."""
     def area(self):
         """Raise a Exception"""
         raise Exception("area() is not implemented")
