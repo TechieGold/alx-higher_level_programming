ce5810e5bf2644b3bfe488caed6ac210f7d984cf
TechieGold <techiegold101@gmail.com>
2023-09-19 01:56:59 +0100

    correct SHE-bang

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 models.rectangle
 ===============
@@ -95,3 +95,7 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
+            
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y}- {self.width}/{self.height}"
