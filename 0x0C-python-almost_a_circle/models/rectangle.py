cfa7369dc6745f27a724c19e94208b97c5aa7533
TechieGold <techiegold101@gmail.com>
2023-09-19 02:01:31 +0100

    implement pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,10 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
-            
+
     def __str__(self):
         """Return a string representation of the Rectangle."""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y}- {self.width}/{self.height}"
+        return (
+            f"[Rectangle] ({self.id}) "
+            f"{self.x}/{self.y} - {self.width}/{self.height}"
+        )
