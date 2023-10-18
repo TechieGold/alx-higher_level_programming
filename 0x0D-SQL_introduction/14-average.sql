c7c3286519eb28d100848bf7a3278ae63ad9e2f6
Gold Israel <techiegold101@gmail.com>
2023-10-18 21:31:30 +0100

    calculate score average and store in an alias

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- A script that computes the score average of all records in the table second_table.
+-- And store the result in an alias (average).
+SELECT AVG(score) as average
+FROM `second_table`
