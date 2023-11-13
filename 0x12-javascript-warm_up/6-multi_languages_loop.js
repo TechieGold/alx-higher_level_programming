18c8f4b7dc5708a1a22b558675450288a5361a84
Gold Israel <techiegold101@gmail.com>
2023-11-13 15:23:38 +0100

    add a script that prints lines by using an array of string and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+    Write a script that prints 3 lines: but by using an array of string and a loop
+    The first line: “C is fun”
+    The second line: “Python is cool”
+    The third line: “JavaScript is amazing”
+*/
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
