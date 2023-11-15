0cbb7548c37089389fb6fc8d082de5f6cff45420
Gold Israel <techiegold101@gmail.com>
2023-11-15 19:53:32 +0100

    Refactor Rectangle constructor for positive dimensions

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,22 +2,19 @@
 
 /*
     A class Rectangle that defines a rectangle:
-        You must use the class notation for defining your class
-        The constructor must take 2 arguments w and h
-        Initialize the instance attribute width with the value of w
+         You must use the class notation for defining your class
+       The constructor must take 2 arguments w and h
+         Initialize the instance attribute width with the value of w
         Initialize the instance attribute height with the value of h
 
-        If w or h is equal to 0 or not a positive integer, create an empty object
-*/
+         If w or h is equal to 0 or not a positive integer, create an empty object
+ */
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
-}
-
-module.exports = Rectangle;
+};
