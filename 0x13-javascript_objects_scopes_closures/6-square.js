b4168d5425b13b7781c34c75a8e969cef915f72b
Gold Israel <techiegold101@gmail.com>
2023-11-16 06:13:36 +0100

    Add charPrint(c) method to Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// A class Square that defines a square and inherits from Square of 5-square.js.
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
+  }
+}
+module.exports = Square;
