640b7462b7ec85531cd3d1d9a8e8a2cb101ffa38
TechieGold <techiegold101@gmail.com>
2023-11-22 12:19:59 +0100

    use list comp instead

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,19 +6,16 @@ from the database hbtn_0e_0_usa
 
 Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
 """
-import MySQLdb as DB
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db_connect = DB.connect(host='localhost', port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], database=sys.argv[3])
-    cursor = db_connect.cursor()
-    sql = "SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`"
-    cursor.execute(sql)
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
 
-    states_with_n = cursor.fetchall()
+    [print(state_with_n) for state_with_n in cur.fetchall()]
 
-    for state in states_with_n:
-        print(state)
-    cursor.close()
-    db_connect.close()
+    cur.close()
+    db.close()
