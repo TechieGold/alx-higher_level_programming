9404c35b43acb01b21882228afc7dbaaf46a014e
TechieGold <techiegold101@gmail.com>
2023-11-22 10:43:01 +0100

    use simple structure

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,20 +7,14 @@ where the name matches the provided argument.
 Usage: ./2-my_filter_states.py <mysql_username> <mysql_password>
 <database_name> <state_to_find>
 """
-import MySQLdb as DB
+import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db_connect = DB.connect(host='localhost', port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], database=sys.argv[3])
-    cursor = db_connect.cursor()
-    sql = "SELECT * FROM `states` WHERE `name` = %s ORDER BY `id`"
-    state_to_find = sys.argv[4]
-    cursor.execute(sql, (state_to_find,))
-
-    matching_states = cursor.fetchall()
-
-    for state in matching_states:
-        print(state)
-    cursor.close()
-    db_connect.close()
+if __name__ == "__main__":
+    db_connect = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                                 db=sys.argv[3])
+    cur = db_connect.cursor()
+    cur.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
