99be08e102cac059774facd9ef861931e16ac41d
TechieGold <techiegold101@gmail.com>
2023-11-22 12:14:22 +0100

    use list comprehension to print states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,15 @@ This script lists all states from the database hbtn_0e_0_usa.
 
 Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
 """
-import MySQLdb as DB
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db_connect = DB.connect(host='localhost', port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], database=sys.argv[3])
-    cursor = db_connect.cursor()
-    sql = "SELECT * FROM `states` ORDER BY `id`"
-    cursor.execute(sql)
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cur.fetchall()]
 
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-    cursor.close()
-    db_connect.close()
+    cur.close
+    db.close()
