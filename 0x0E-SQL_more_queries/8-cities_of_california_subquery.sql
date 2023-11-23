e2421900c8222559a220e7f5d06970e313365826
TechieGold <techiegold101@gmail.com>
2023-11-23 17:22:23 +0100

    use subquery to list cities

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Script to list all cities in California without using JOIN
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
