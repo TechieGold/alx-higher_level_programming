6d146e485b5ebda26fc7e4030c3bf3debf4a6c17
TechieGold <techiegold101@gmail.com>
2023-11-23 17:33:02 +0100

    update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,9 @@
--- Script to list all cities in California without using JOIN
-
-USE hbtn_0d_usa;
-
-SELECT cities.id, cities.name
-FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+    (SELECT `id`
+	FROM `states`
+	WHERE `name` = "California")
+ ORDER BY `id`;
