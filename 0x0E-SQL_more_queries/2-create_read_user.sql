008ecbac5558e5044f264913ee9cb6ac518df75a
TechieGold <techiegold101@gmail.com>
2023-11-23 12:51:56 +0100

    add comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,3 @@
+-- This script creates a database "hbtn_0d_2", and also create a user "user_0d_2".
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
