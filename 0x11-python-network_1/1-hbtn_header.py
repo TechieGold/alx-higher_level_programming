31905a95d92a8cae9826641cfb1babde85b58df5
TechieGold <techiegold101@gmail.com>
2023-12-04 09:00:20 +0100

    unified script format

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,14 @@
 This script fetches the value of X-Request-Id variable
 from the header of the response.
 """
-from urllib.request import urlopen
 import sys
+from urllib.request import Request
+from urllib.request import urlopen
+
 url = sys.argv[1]
 
 if __name__ == "__main__":
-    with urlopen(url) as response:
-        content = response.getheader('X-Request-Id')
-        print(content)
+    request = Request(url)
+    with urlopen(request) as response:
+        x_request_id = dict(response.headers).get("X-Request-Id")
+        print(x_request_id)
