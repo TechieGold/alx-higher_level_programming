56c9a04cc1ba0e3666d9b6a554f6714f19f7c4ca
TechieGold <techiegold101@gmail.com>
2024-01-11 07:25:34 +0100

    use addClass to add element feature

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+// This script adds the class red of the header element
+// when the user click on the tag  DIV#red_header
+
+$('DIV#red_header').click(function () {
+    $('HEADER').addClass('red');
+  });
+  
