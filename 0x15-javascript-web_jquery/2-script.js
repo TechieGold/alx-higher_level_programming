196fb8e6a5921b64d6a29ae0d1f0a5a33e56c06f
TechieGold <techiegold101@gmail.com>
2024-01-11 07:26:08 +0100

    use simple   syntax

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,10 +1,6 @@
 // This script updates the text color of the header element
 // When the user click on the tag  DIV#red_header
 
-$(document).ready(function() {
-    $('#red_header').click(function() {
-        const header = $('header');
-
-        header.css('color', '#FF0000');
-    });
+$('DIV#red_header').click(function () {
+  $('HEADER').css('#FF0000');
 });
