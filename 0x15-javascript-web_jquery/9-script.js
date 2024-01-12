849dfd50bf7f7408a9639b5dc6a591e023ced051
TechieGold <techiegold101@gmail.com>
2024-01-12 12:07:52 +0100

    add script that fetches and display data from a url

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+// This script fetches a value from URL and display the content.
+$(document).ready(function() {
+    $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function(data) {
+        $('DIV#hello').text(data.hello);
+    });
+});
